8878b61ff75176be7b47b22724b7c979accb41bf
vladiacob <vlad.iacob@hackajob.co>
2022-09-11 18:56:57 +0300

    Update HelloController.java

--- src/main/java/uk/co/argos/interview/controller/HelloController.java
+++ src/main/java/uk/co/argos/interview/controller/HelloController.java
@@ -1,6 +1,7 @@
 package uk.co.argos.interview.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import uk.co.argos.interview.service.PokedexService;
 import uk.co.argos.interview.entity.Pokemon;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class HelloController {
     
