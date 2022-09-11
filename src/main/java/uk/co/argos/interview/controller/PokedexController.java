11c8a587275cba6b3f487bd89a2367e04ffe946b
vladiacob <vlad.iacob@hackajob.co>
2022-09-11 18:57:20 +0300

    Update PokedexController.java

--- src/main/java/uk/co/argos/interview/controller/PokedexController.java
+++ src/main/java/uk/co/argos/interview/controller/PokedexController.java
@@ -1,5 +1,8 @@
 package uk.co.argos.interview.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PokedexController {
 
     // TODO: Implement an endpoint that allows someone to retrieve a pokemon by english name
