3dcdf067fcb44fa6b25495fcc188595fad70efd5
vladiacob <vlad.iacob@hackajob.co>
2022-09-11 14:37:43 +0300

    Update PokedexService.java

--- src/main/java/uk/co/argos/interview/service/PokedexService.java
+++ src/main/java/uk/co/argos/interview/service/PokedexService.java
@@ -1,13 +1,15 @@
 package uk.co.argos.interview.service;
 
+import uk.co.argos.interview.entity.Pokemon;
+
 public class PokedexService {
 
     /**
      * There is a JSON file pokedex.json with all the pokemons available. This service will accept the pokemon name in
      * english as parameter, and it will return all the details about the pokemon received.
      */
-    public String get(String name) {
-        return "";
+    public Pokemon get(String name) {
+        return new Pokemon();
     }
 
 }
